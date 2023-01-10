7b451f2a9de3036d9b126350dc4d625e3525cdde
Brenda Moraga <brenda.moraga.meirelles@gmail.com>
2023-01-10 14:04:48 -0300

    estructura cambio html para register

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,26 +2,35 @@
 import { onNavigate } from '../main.js';
 
 export const Register = () => {
-    const div = document.createElement('div');
-    const title = document.createElement('h2');
-    const button = document.createElement('button');
-    const buttonBack = document.createElement('button');
-    const inputEmail = document.createElement('input');
-    const inputPass = document.createElement('input');
+    const divRegis = document.createElement('div');
+    divRegis.classList.add('contenedorRegis');
+    divRegis.innerHTML = `<div class="contendorRegis">
+    <input type="text" placeholder="Ingresa nombre de usuario" class="inputRegis">
+    <input type="text" placeholder="Crear contraseña" class="inputRegis">
+    <input type="text" placeholder="Correo electrónico" class="inputRegis">
+    <select name="SelecRegis" id="btnSoy" class="btnList">
+      <option> Músico Profesional</option>
+      <option> Banda</option>
+      <option> Autor/Compositor</option>
+      <option> Fans/Espectador</option>
+    </select>
+    <button onclick="" class="btn-InicioSesion">Registrarse</button>
+    <a href="" class="recuperarContraseña">Regresar</a>
+    <hr />`
 
-    button.textContent = 'Crear cuenta';
-    buttonBack.textContent = 'Regresa';
-    title.textContent = 'Registro de nueva cuenta';
 
-    button.addEventListener('click', () => {
-        onNavigate('/login');
-    });
-    buttonBack.addEventListener('click', () => {
-        onNavigate('/');
-    });
-
-    div.append(title, inputEmail, inputPass, button, buttonBack);
-
-    return div;
+    return divRegis;
 };
 
+// button.textContent = 'Crear cuenta';
+// buttonBack.textContent = 'Regresa';
+// title.textContent = 'Registro de nueva cuenta';
+
+// button.addEventListener('click', () => {
+//     onNavigate('/login');
+// });
+// buttonBack.addEventListener('click', () => {
+//     onNavigate('/');
+// });
+
+// div.append(title, inputEmail, inputPass, button, buttonBack);
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,38 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "/firebase/app"; // conexion global no necesita importar modulo
-// import { getAnalytics } from "/firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // import { initializeApp } from "firebase/app";
-//  import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-// // // TODO: Replace the following with your app's Firebase project configuration
-// // // See: https://firebase.google.com/docs/web/learn-more#config-object
-// // const firebaseConfig = {
-// //   // ...
-// // };
-
-// // // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-
-
-// // // Initialize Firebase Authentication and get a reference to the service
-// // const auth = getAuth(app);
-
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCXUFaEX3vpniO0aQVvMMzlf6_TTu0rTKQ",
-//     authDomain: "app-view-my-music.firebaseapp.com",
-//     projectId: "app-view-my-music",
-//     storageBucket: "app-view-my-music.appspot.com",
-//     messagingSenderId: "556207282264",
-//     appId: "1:556207282264:web:9568c17056b0a3a4a7d22d",
-//     measurementId: "G-9QYJ98EZCD"
-// };
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
