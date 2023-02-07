797fd7ddfb25be4dec24185829a35a131b28cea3
Brenda Moraga <brenda.moraga.meirelles@gmail.com>
2023-02-07 10:26:16 -0300

    prueba

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -17,8 +17,10 @@ export const Wall = () => {
   <textarea name="" id="" class="textPost" placeholder="Escribe una descripcion para tu publicacion..."></textarea>
   <img class="fotoConcert" id="postUs" src="../img/flayer.png">
 </div>
-<button id="like" class="corazon"> </button>
+<div class="btns">
+<button id="like" class="corazon"></button>
 <button id="entrada" class="ticket"></button>
+</div>
 <form id="formComentario" class="formComen">
   <textarea name="" id="textoComent" class="textPost" placeholder="escribe un comentario..."></textarea>
   <button id="comentar" class="enviarComentario"> Comentar </button>
@@ -31,6 +33,7 @@ export const Wall = () => {
   return divWall;
 };
 
+
 window.addEventListener("DOMContentLoaded", async () => {
   
   const divComentario = document.querySelector('#contenedorComentario');
@@ -39,7 +42,7 @@ window.addEventListener("DOMContentLoaded", async () => {
   let editStatus = false;
   let id = "";
   
-  onGetTasks((querySnapshot) => {
+  onGetTasks((querySnapshot) => { 
     divComentario.innerHTML = "";
 
     querySnapshot.forEach((doc) => {
