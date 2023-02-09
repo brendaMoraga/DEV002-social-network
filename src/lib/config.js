1ea0174fd2a15497db572d1032b67faf588cd21f
Brenda Moraga <brenda.moraga.meirelles@gmail.com>
2023-02-09 14:43:39 -0300

    cambios para deploy

---
+++ src/lib/config.js
@@ -0,0 +1,9 @@
+
+ export const firebaseConfig = {
+    apiKey: 'AIzaSyAU0Ta8ZyJvb3griwsGlC-PcaqRNjHkMnM',
+    authDomain: 'view-my-music.firebaseapp.com',
+    projectId: 'view-my-music',
+    storageBucket: 'view-my-music.appspot.com',
+    messagingSenderId: '137287079012',
+    appId: '1:137287079012:web:73908c3667805061763b71',
+  };
